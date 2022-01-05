97b74459c10a350ba8a3cfccd997db05d6558347
Park Hyeojun <hj.park@cashwalk.io>
2022-01-05 15:34:22 +0900

    Sperate brackets and quotes. add stack calcuate open and close

--- traverse.el
+++ traverse.el
@@ -1,13 +1,16 @@
-(defconst jest--pairs '('("'" "'") '("\"" "\"") '("`" "`")
-                        '("(" ")") '("{" "}") '("[" "]") '("<" ">"))
-  "open and closed pairs")
+(defconst jest--brackets '("(" ")") '("{" "}") '("[" "]") '("<" ">"))
+(defconst jest--strings '('("'" "'") '("\"" "\"") '("`" "`")))
+
 (defun jest--get-buffer-to-point () 
   (buffer-substring 1 (+ (point) 1)))
 
+;; TODO remove text closed with pairs but need to remain complete string
 (defun jest--remove-folded-range (text) 
-  text)
+  (let ((stack '()))
+    text))
 
 (defun jest--get-describe-name-at-point ()
+  "Get all merged names in hierarchical describe til current position"
   (let ((text (jest--remove-folded-range (jest--get-buffer-to-point))))
     (string-match "describe\(\\(.*\\)," text)
     (when-let (describe-name (match-string 1 text))
