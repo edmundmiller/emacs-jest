831bec9bc01b35959824ae6a4a585256bd8e5b32
Park Hyeonjun <phjosh06@gmail.com>
2022-01-14 01:12:18 +0900

    Working on function jest--remove-folded-range

--- traverse.el
+++ traverse.el
@@ -1,12 +1,22 @@
-(defconst jest--brackets '("(" ")") '("{" "}") '("[" "]") '("<" ">"))
-(defconst jest--strings '('("'" "'") '("\"" "\"") '("`" "`")))
+(defconst jest--brackets '(("(" ")") '("{" "}") '("[" "]") '("<" ">")))
+(defconst jest--quotes '('"'" "\"" "`"))
 
 (defun jest--get-buffer-to-point () 
   (buffer-substring 1 (+ (point) 1)))
 
+(defun jest--check-char-bracket (character)
+  ())
+(defun jest--check-char-quote (character)
+  ())
+
 ;; TODO remove text closed with pairs but need to remain complete string
 (defun jest--remove-folded-range (text) 
   (let ((stack '()))
+    (dotimes (i (length text))
+      ;; (print (char-equal ?t (substring text i (+ i 1))))
+      (print (char-equal ?t (c-int-to-char (aref text i))))
+      ;; (print (aref text i))
+      )
     text))
 
 (defun jest--get-describe-name-at-point ()
---
+++ traverset-test.el
@@ -0,0 +1,17 @@
+(require 'traverse.el)
+(require 'ert)
+
+(ert-deftest jest-test--remove-folded-range ()
+  (should (equal "test text" (jest--remove-folded-range "test text"))))
+
+
+(ert-deftest jest-test--remove-text-in-bracket ()
+  (should (equal "test () text" (jest--remove-folded-range "test (be removed) text")))
+  (should (equal "test {} text" (jest--remove-folded-range "test {be removed} text")))
+  (should (equal "test [] text" (jest--remove-folded-range "test [be removed] text")))
+  (should (equal "test <> text" (jest--remove-folded-range "test <be removed> text"))))
+
+(ert-deftest jest-test--wont-remove-text-quotes ()
+  (should (equal "test '(hi)' text" (jest--remove-folded-range "test '(hi)' text")))
+  (should (equal "test \"<hi>\" text" (jest--remove-folded-range "test \"<hi>\" text")))
+  (should (equal "test `<hi>` text" (jest--remove-folded-range "test `<hi>` text"))))
